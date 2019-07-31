b74f732d8acb991fddb0cb87ba7fe8be239cc98a
TheWaterIsAlive <ajfry203@gmail.com>
2019-08-01 08:28:23 +1200

    First Commit

---
+++ Assets/Scripts/bunnyControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bunnyControl : MonoBehaviour
+
+{
+    private Rigidbody2D myRigidBody2D;
+    public float bunnyForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+
+            myRigidBody2D.AddForce(transform.up * bunnyForce);
+
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/bunnyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+
+            myRigidBody2D.AddForce(transform.up * 500f);
+
+        }
+        
+    }
+}
